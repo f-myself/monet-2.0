b9b05665189955b4b5d44e549210833dbd5b0072
f-myself <mchabanov@acutenet.com>
2019-12-14 19:19:12 +0200

    add hidding and animation on index

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const $faqCards = $('.btn-card');
+const $mainNavbar = $('#js-main-navbar');
 
+
+$(window).scroll(() => {
+    if(pageYOffset > 400) {
+        $mainNavbar.slideUp("slow");
+    } else {
+        $mainNavbar.slideDown("slow");
+    }
+})
 
 $('#accordionExample').on('show.bs.collapse', function () {
     console.log('showing!');
