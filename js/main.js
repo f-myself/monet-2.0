1f412f2caac159a85d9e072215b4ee0b06628c31
f-myself <mchabanov@acutenet.com>
2019-12-14 20:01:33 +0200

    fixed first screen and menu issues

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 const $faqCards = $('.btn-card');
 const $mainNavbar = $('#js-main-navbar');
-
+const $secondaryNavbar = $('#js-secondary-navbar');
 
 $(window).scroll(() => {
     if(pageYOffset > 400) {
-        $mainNavbar.slideUp("slow");
+        $mainNavbar.slideUp(300, () => {
+            $secondaryNavbar.slideDown(300);
+        });
     } else {
-        $mainNavbar.slideDown("slow");
+        $secondaryNavbar.slideUp(300, () => {
+            $mainNavbar.slideDown(300);
+        });
+
     }
 })
 
